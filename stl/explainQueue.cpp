09ac2f0b98da17981f30693985f89de0f1236e84
vipin pratap <s.pvimalpratap9458@gmail.com>
2026-06-24 19:03:52 +0530

    added queue in stl

---
+++ stl/explainQueue.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainQueue(){
+	 // FIFO -> First In First Out
+	 
+	queue<int> q;
+	q.push(1); // {1}
+	q.push(2); // {1,2}
+	q.emplace(4) // {1,2,4}
+	
+	q.back() += 5;
+	
+	// q is {1,2,9}
+	cout << q.back(); // 9
+	
+	cout << q.front(); // 1
+	
+	q.pop(); // {2,9}
+	cout << q.front(); // 2
+	
+	// size, swap , empty same as stack
+}
